cfd12b1ea33578016157b4a973601059e4c11bed
Tristan Engel <121832013+trengel@users.noreply.github.com>
2023-03-07 16:46:30 -0500

    Create insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+# Empty tables before reinserting data
+$($PSQL "TRUNCATE TABLE games, teams;")
